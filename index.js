70c11165ca816ce55ee2f0415c51f0bdfcdca515
Chad Zuniga <chad.zuniga10@gmail.com>
2021-06-03 09:55:23 -0600

    Initial Graphql schema

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from 'express'
+import schema from './schema'
+import { graphqlHTTP } from 'express-graphql'
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('graphql')
 })
+
+const root = {
+    hello: () => "Hi"
+}
+
+app.use('/graphql', graphqlHTTP ({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}))
 
 app.listen(8080, () => console.log('Running server on 8080'))
